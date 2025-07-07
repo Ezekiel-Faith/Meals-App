3817c227a960edfb25a095467fde1bc4915575ff
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-07-07 04:30:41 +0100

    working with React Navigation

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Meals Categories' component={CategoriesScreen} />
+        </Stack.Navigator>
+
+        {/* <Stack.Navigator>
+          <Stack.Screen
+            name='Meals Categories'
+            component={CategoriesScreen}
+            options={{
+              headerStyle: { backgroundColor: '#351401' },
+              headerTintColor: 'white',
+              // title: 'All Categories',
+            }}
+          />
+        </Stack.Navigator> */}
+      </NavigationContainer>
     </>
   );
 }
